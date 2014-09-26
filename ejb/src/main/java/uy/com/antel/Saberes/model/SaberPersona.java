04845d3a26a14f226873acba0d93742a3966329e
jsanguinetti <jsanguinetti@jasanguinetti>
2014-09-26 11:31:12 -0300

    Actualizo atributo Saber en POJO de SaberPersona

--- ejb/src/main/java/uy/com/antel/Saberes/model/SaberPersona.java
+++ ejb/src/main/java/uy/com/antel/Saberes/model/SaberPersona.java
@@ -3,12 +3,12 @@ package uy.com.antel.Saberes.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -24,6 +24,18 @@ public abstract class SaberPersona implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@ManyToOne(optional=false) 
+    @JoinColumn(name="idSaber", nullable=false, updatable=false)
+	private Saber saber;
+	
+	public Saber getSaber() {
+		return saber;
+	}
+
+	public void setSaber(Saber saber) {
+		this.saber = saber;
+	}
+
 	@Temporal(TemporalType.DATE)
 	private Date fechaInicio;
 	
@@ -52,6 +64,7 @@ public abstract class SaberPersona implements Serializable{
 		result = prime * result
 				+ ((fechaInicio == null) ? 0 : fechaInicio.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((saber == null) ? 0 : saber.hashCode());
 		return result;
 	}
 
@@ -84,9 +97,14 @@ public abstract class SaberPersona implements Serializable{
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (saber == null) {
+			if (other.saber != null)
+				return false;
+		} else if (!saber.equals(other.saber))
+			return false;
 		return true;
 	}
-	
-	
+
+		
 	
 }
