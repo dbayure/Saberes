d6339d73ed1167760b8621710a5d967b5dcb5655
tvdi <tvdi@tvdi-ginga>
2015-03-06 10:35:33 -0200

    validacion

--- web/src/main/java/uy/com/antel/Saberes/bean/PersonaBean.java
+++ web/src/main/java/uy/com/antel/Saberes/bean/PersonaBean.java
@@ -1,23 +1,23 @@
 package uy.com.antel.Saberes.bean;
 
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
-
+import java.util.Properties;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.xml.rpc.ServiceException;
-
 import org.jboss.security.SecurityContextAssociation;
 import org.primefaces.event.RowEditEvent;
 import org.primefaces.model.UploadedFile;
-
 import WebServices.sgp.antel.com.DatoPer;
 import uy.com.antel.Saberes.controller.RegistroNoCorporativo;
 import uy.com.antel.Saberes.controller.RegistroPersona;
@@ -26,7 +26,6 @@ import uy.com.antel.Saberes.model.NoCorporativo;
 import uy.com.antel.Saberes.model.Persona;
 import uy.com.antel.Saberes.model.Rol;
 import uy.com.antel.Saberes.model.SaberPersona;
-import uy.com.antel.Saberes.model.TipoSaber;
 import uy.com.iantel.in.wsi_prod.WSsgp.services.WSDatoPer.WsDatosPer;
 import uy.com.iantel.in.wsi_prod.WSsgp.services.WSDatoPer.WsDatosPerService;
 import uy.com.iantel.in.wsi_prod.WSsgp.services.WSDatoPer.WsDatosPerServiceLocator;
@@ -36,41 +35,46 @@ import uy.com.iantel.in.wsi_prod.WSsgp.services.WSDatoPer.WsDatosPerServiceLocat
 public class PersonaBean {
 
 	private String usuario;
-	
+	private long idPersona;
+	private String rutaPDF;
+	private String rutaIMG;
+	boolean mostrar = false;
+	private List<NoCorporativo> listaNoCorporativoPersona = new ArrayList<NoCorporativo>();
+
 	@Inject
 	private RegistroPersona registroPersona;
-	
+
 	@Inject
 	private RegistroNoCorporativo registroNoCorporativo;
-	
+
 	@Inject
 	private RegistroNoCorporativo registroCorporativo;
-	
+
 	private UploadedFile file;
-	
+
 	public UploadedFile getFile() {
-        return file;
-    }
- 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-	
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
 	public void registrar() {
 		FacesMessage msg;
-	
+
 		try {
-			
+
 			Persona aPersona;
 			if (personaRegistrada())
 				aPersona = registroPersona.buscarPersonaPorUsr(this.usuario);
 			else
 				aPersona = registroPersona.getNewPersona();
-				
-				
+
 			DatoPer datos = obtenerDatosPersona(this.getUsuario());
-			
-			String apellido = datos.getPriape().trim()+" "+datos.getSegape().trim();
+
+			String apellido = datos.getPriape().trim() + " "
+					+ datos.getSegape().trim();
 			String nombre = datos.getNombre().trim();
 			Calendar fNacim = datos.getFNacim();
 			Calendar fIngreso = datos.getFIngre();
@@ -90,7 +94,6 @@ public class PersonaBean {
 			String fax = datos.getFaxUnidad();
 			String piso = datos.getPlanta();
 			String email = datos.getEmail();
-
 			aPersona.setApellido(apellido);
 			aPersona.setNombre(nombre);
 			aPersona.setFechaIngreso(fIngreso.getTime());
@@ -112,31 +115,33 @@ public class PersonaBean {
 			aPersona.setPiso(piso);
 			aPersona.setCorreo(email);
 			aPersona.setUsuario(this.usuario);
-			
+
 			if (personaRegistrada())
 				registroPersona.modificar(aPersona);
 			else
 				registroPersona.registro();
-			
+
 			this.setPersonaBean(this.usuario);
-			
-//			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Se registró ", "con éxito!");  
-//	        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+			// msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+			// "Se registró ", "con éxito!");
+			// FacesContext.getCurrentInstance().addMessage(null, msg);
 		}
 
 		catch (Exception e) {
-			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Error al registrar ", "");	
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Error al registrar ", "");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		}
-		 
+
 	}
-	
+
 	public boolean personaRegistrada() {
 		return (registroPersona.buscarPersonaPorUsr(this.usuario) != null);
 	}
 
-	private DatoPer obtenerDatosPersona(String usuario)  {
-		
+	private DatoPer obtenerDatosPersona(String usuario) {
+
 		WsDatosPerService service = new WsDatosPerServiceLocator();
 		WsDatosPer llamada;
 		try {
@@ -144,150 +149,231 @@ public class PersonaBean {
 			String sCedula = getCi(usuario);
 			String sAppl = "SABERES";
 			String sPass = "SABERES";
-			
+
 			return llamada.getDatosPer(sCedula, sAppl, sPass);
-			
+
 		} catch (ServiceException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 		return null;
-
-
 	}
-	
-	public void setPersonaBean (String usr ){
+
+	public void setPersonaBean(String usr) {
 		registroPersona.setPersonaPorUsr(usr);
 	}
-	
+
 	private String getCi(String ci) {
-		List<String> conv = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k");
-		if (ci.length() < 7){
-			return "a"+ci.substring(1);
-		}else{
-			int primerLetra = conv.indexOf(ci.substring(0,1));
-			return primerLetra + ci.substring(1); 
+		List<String> conv = Arrays.asList("a", "b", "c", "d", "e", "f", "g",
+				"h", "i", "j", "k");
+		if (ci.length() < 7) {
+			return "a" + ci.substring(1);
+		} else {
+			int primerLetra = conv.indexOf(ci.substring(0, 1));
+			return primerLetra + ci.substring(1);
 		}
-		
+
 	}
 
-	public void onCancel(RowEditEvent event) {  
-        FacesMessage msg = new FacesMessage("Se canceló modificar ", ((Rol) event.getObject()).getRol());  
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-    }
+	public void onCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Se canceló modificar ",
+				((Rol) event.getObject()).getRol());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 
 	public String getUsuario() {
 		return usuario;
 	}
 
-	public List<NoCorporativo> obtenerSaberesPersona(long id){
+	public List<NoCorporativo> obtenerSaberesPersona(long id) {
 		Persona p = registroPersona.encontrarPorId(id);
 		List<NoCorporativo> listanc = new ArrayList<NoCorporativo>();
-		for (SaberPersona sp : p.getSaberes()){
-			if(sp instanceof NoCorporativo){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp.getId());
-				if(!nc.isValidado()){
+		for (SaberPersona sp : p.getSaberes()) {
+			if (sp instanceof NoCorporativo) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp
+						.getId());
+				if (!nc.isValidado()) {
 					listanc.add(nc);
 				}
 			}
 		}
 		return listanc;
 	}
-	
-	public List<NoCorporativo> obtenerNoCorporativos(){
+
+	public List<NoCorporativo> obtenerNoCorporativos() {
 		String userName = SecurityContextAssociation.getPrincipal().getName();
 		Persona p = registroPersona.buscarPersonaPorUsr(userName);
 		List<NoCorporativo> listanc = new ArrayList<NoCorporativo>();
-		for (SaberPersona sp : p.getSaberes()){
+		for (SaberPersona sp : p.getSaberes()) {
 			String ts = sp.getSaber().getTipoSaber().getNombre();
-			if(ts.contains("Formal No Corporativo")){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp.getId());
+			if (ts.contains("Formal No Corporativo")) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp
+						.getId());
 				listanc.add(nc);
 			}
 		}
 		return listanc;
 	}
-	
-	public List<Corporativo> obtenerCorporativos(){
+
+	public List<Corporativo> obtenerCorporativos() {
 		String userName = SecurityContextAssociation.getPrincipal().getName();
 		Persona p = registroPersona.buscarPersonaPorUsr(userName);
 		List<Corporativo> listanc = new ArrayList<Corporativo>();
-		for (SaberPersona sp : p.getSaberes()){
+		for (SaberPersona sp : p.getSaberes()) {
 			String ts = sp.getSaber().getTipoSaber().getNombre();
-			if(ts.contains("Formal Corporativo")){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				Corporativo nc = registroCorporativo.obtenerCorpPorID(sp.getId());
+			if (ts.contains("Formal Corporativo")) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				Corporativo nc = registroCorporativo.obtenerCorpPorID(sp
+						.getId());
 				listanc.add(nc);
 			}
 		}
 		return listanc;
 	}
-	
-	public List<NoCorporativo> obtenerCurriculares(){
+
+	public List<NoCorporativo> obtenerCurriculares() {
 		String userName = SecurityContextAssociation.getPrincipal().getName();
 		Persona p = registroPersona.buscarPersonaPorUsr(userName);
 		List<NoCorporativo> listanc = new ArrayList<NoCorporativo>();
-		for (SaberPersona sp : p.getSaberes()){
+		for (SaberPersona sp : p.getSaberes()) {
 			String ts = sp.getSaber().getTipoSaber().getNombre();
-			if(ts.contains("Formal Curricular")){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp.getId());
+			if (ts.contains("Formal Curricular")) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp
+						.getId());
 				listanc.add(nc);
 			}
 		}
 		return listanc;
 	}
-	
-	public List<NoCorporativo> obtenerCursos(){
+
+	public List<NoCorporativo> obtenerCursos() {
 		String userName = SecurityContextAssociation.getPrincipal().getName();
 		Persona p = registroPersona.buscarPersonaPorUsr(userName);
 		List<NoCorporativo> listanc = new ArrayList<NoCorporativo>();
-		for (SaberPersona sp : p.getSaberes()){
+		for (SaberPersona sp : p.getSaberes()) {
 			String ts = sp.getSaber().getTipoSaber().getNombre();
-			if(ts.contains("No Formal Cursos")){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp.getId());
+			if (ts.contains("No Formal Cursos")) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp
+						.getId());
 				listanc.add(nc);
 			}
 		}
 		return listanc;
 	}
-	
-	public List<NoCorporativo> obtenerConocimientos(){
+
+	public List<NoCorporativo> obtenerConocimientos() {
 		String userName = SecurityContextAssociation.getPrincipal().getName();
 		Persona p = registroPersona.buscarPersonaPorUsr(userName);
 		List<NoCorporativo> listanc = new ArrayList<NoCorporativo>();
-		for (SaberPersona sp : p.getSaberes()){
+		for (SaberPersona sp : p.getSaberes()) {
 			String ts = sp.getSaber().getTipoSaber().getNombre();
-			if(ts.contains("No Formal Conocimientos")){
-				System.out.println("Nombre del saber seleccionado " + sp.getSaber().getNombre());
-				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp.getId());
+			if (ts.contains("No Formal Conocimientos")) {
+				System.out.println("Nombre del saber seleccionado "
+						+ sp.getSaber().getNombre());
+				NoCorporativo nc = registroNoCorporativo.obtenerPorID(sp
+						.getId());
 				listanc.add(nc);
 			}
 		}
 		return listanc;
 	}
-	
-	public String convertirBoolean(Boolean var){
+
+	public String convertirBoolean(Boolean var) {
 		if (var)
 			return "Si";
 		else
-			return "No";		
-	}	
-	
-	public boolean faltaValidar (long id){
+			return "No";
+	}
+
+	public boolean faltaValidar(long id) {
 		boolean falta = false;
-		System.out.println("Esta preguntando por la validación");
-		if(obtenerSaberesPersona(id).size() > 0){
+		if (obtenerSaberesPersona(id).size() > 0) {
 			falta = true;
 		}
 		return falta;
 	}
-	
+
+	public void validarUnaPersona(long id) {
+		listaNoCorporativoPersona = obtenerSaberesPersona(id);
+	}
+
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+
+	public List<NoCorporativo> getListaNoCorporativoPersona() {
+		return listaNoCorporativoPersona;
+	}
+
+	public void setListaNoCorporativoPersona(
+			List<NoCorporativo> listaNoCorporativoPersona) {
+		this.listaNoCorporativoPersona = listaNoCorporativoPersona;
+	}
+
+	public long getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(long idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public void asignarId(long id) {
+		this.idPersona = id;
+		obtenerRuta();
+	}
+
+	public boolean isMostrar() {
+		return mostrar;
+	}
+
+	public void setMostrar(boolean mostrar) {
+		this.mostrar = mostrar;
+	}
+
+	public void obtenerRuta() {
+		Properties p = new Properties();
+		String archivoPropiedades = System.getProperty("user.dir")
+				+ "/Conf/app-properties/saberes.properties";
+		System.out.println("valor del archivo de propiedades **************"
+				+ archivoPropiedades);
+		try {
+			p.load(new FileInputStream(archivoPropiedades));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		rutaPDF = p.getProperty("urlPDF");
+		rutaIMG = p.getProperty("urlIMG");
+		System.out.println("ruta seleccionada para archivos pdf " + rutaPDF);
+		System.out.println("ruta seleccionada para archivos img " + rutaIMG);
+	}
+
+	public String getRutaIMG() {
+		return rutaIMG;
+	}
+
+	public void setRutaIMG(String rutaIMG) {
+		this.rutaIMG = rutaIMG;
+	}
+
+	public String getRutaPDF() {
+		return rutaPDF;
+	}
+
+	public void setRutaPDF(String rutaPDF) {
+		this.rutaPDF = rutaPDF;
+	}
+
 }
